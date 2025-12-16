ae1ebd54352f0f761a5e312222a5da10f056f5fa
pyadminru <zh.vitaliy92@yandex.ru>
2025-12-17 00:25:22 +0300

    Формирование документации к коду

--- YouTube/YouTube.py
+++ YouTube/YouTube.py
@@ -5,6 +5,14 @@ from proxy.proxy import setup_proxy
 
 
 def download_video(url):
+    """
+    Скачивает видео с YouTube в максимальном доступном разрешении.
+    
+    Функция настраивает прокси, подключается к YouTube с использованием OAuth,
+    получает поток видео в наивысшем разрешении и скачивает его в указанную директорию.
+    
+    :param url: Ссылка на видео YouTube
+    """
     setup_proxy()
 
     yt = YouTube(url, on_progress_callback=on_progress, use_oauth=True, allow_oauth_cache=True)
