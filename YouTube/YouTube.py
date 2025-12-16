fb49e726df674f7bddadcbb341fbf9143902cec6
pyadminru <zh.vitaliy92@yandex.ru>
2025-12-17 00:11:43 +0300

    :white_check_mark: Добавлена работа с proxy

--- YouTube/YouTube.py
+++ YouTube/YouTube.py
@@ -1,14 +1,14 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-from proxy.proxy import setup_proxy, password, user, port, ip
+from proxy.proxy import setup_proxy
 
 
 def download_video(url):
-    setup_proxy(user, password, ip, port)
+    setup_proxy()
 
-    yt = YouTube(url, on_progress_callback=on_progress)
+    yt = YouTube(url, on_progress_callback=on_progress, use_oauth=True, allow_oauth_cache=True)
     print(yt.title)
 
     ys = yt.streams.get_highest_resolution()
-    ys.download()
+    ys.download(output_path='downloaded_videos', max_retries=10)
--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из файла .env
+load_dotenv()
 
 user = os.getenv("USER")  # Пользователь для прокси
 password = os.getenv("PASSWORD")  # Пароль для прокси
 port = os.getenv("PORT")  # Порт для прокси
 ip = os.getenv("IP")  # IP адрес прокси
 
-def setup_proxy(user, password, ip, port):
-    # Указываем прокси для HTTP и HTTPS
-    os.environ['http_proxy'] = f"http://{user}:{password}@{ip}:{port}"
-    os.environ['https_proxy'] = f"http://{user}:{password}@{ip}:{port}"
+def setup_proxy():
+    # Проверяем, что все необходимые переменные окружения установлены
+    if user and password and ip and port:
+        # Указываем прокси для HTTP и HTTPS
+        os.environ['http_proxy'] = f"http://{user}:{password}@{ip}:{port}"
+        os.environ['https_proxy'] = f"http://{user}:{password}@{ip}:{port}"
+    else:
+        # Если переменные окружения не установлены, пропускаем настройку прокси
+        print("[!] Переменные окружения для прокси не установлены. Продолжаем без прокси.")
